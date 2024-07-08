42c818d57324d97f6fa151f29e385a5b6db57584
Bernard Moses <bernardmoses@Bernards-MacBook-Pro.local>
2024-07-09 00:15:11 +0530

    Initial commit

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -10,7 +10,10 @@ const Footer = () => (
          
         </div>
         <div>
-          <h3 className="text-xl font-bold mb-2 text-blue-300">Menu</h3>
+   
+        </div>
+        <div>
+        <h3 className="text-xl font-bold mb-2 text-blue-300">Menu</h3>
           <ul>
             <li className="mb-1"><a href="#" className="hover:underline">Home</a></li>
             <li className="mb-1"><a href="#" className="hover:underline">About</a></li>
@@ -21,21 +24,13 @@ const Footer = () => (
             <li className="mb-1"><a href="#" className="hover:underline">Contact</a></li>
           </ul>
         </div>
-        <div>
-          <h3 className="text-xl font-bold mb-2 text-blue-300">Location</h3>
-          <p>Lebanon, MO</p>
-          <p>929 Springfield Rd</p>
-        </div>
         <div className="flex flex-col justify-between">
           <div>
-            <h3 className="text-xl font-bold mb-2 text-blue-300">Follow Us</h3>
-            <div className="flex space-x-4">
-              <a href="#" className="hover:opacity-75"><img src="/icons/facebook.png" alt="Facebook" /></a>
-              <a href="#" className="hover:opacity-75"><img src="/icons/twitter.png" alt="Twitter" /></a>
-              <a href="#" className="hover:opacity-75"><img src="/icons/instagram.png" alt="Instagram" /></a>
-              <a href="#" className="hover:opacity-75"><img src="/icons/linkedin.png" alt="LinkedIn" /></a>
-              <a href="#" className="hover:opacity-75"><img src="/icons/youtube.png" alt="YouTube" /></a>
-            </div>
+            <h3 className="text-xl font-bold mb-2 text-blue-300">Location</h3>
+            <p>929 Springfield Rd</p>
+            <p>Lebanon, MO</p>
+            
+           
           </div>
         </div>
       </div>
