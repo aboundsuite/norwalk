9e13a5dda3929bf80ee99da9d525330dcc8f22cd
Bernard Moses <mobile@aboundfi.com>
2024-07-28 23:45:58 +0530

    Initial commit or your specific message

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -8,7 +8,7 @@ const Contact = () => {
         
         <div className="mb-8">
           <h2 className="text-2xl font-bold mb-2 text-blue-700">Call Us</h2>
-          <p className="text-lg text-gray-700 mb-4">417-532-4412</p>
+          <p className="text-lg text-gray-700 mb-4">419-668-4629</p>
           <a href="tel:417-532-4412">
             <button className="bg-blue-900 hover:bg-blue-700 text-white px-8 py-3 rounded text-xl">
               Call Now
@@ -18,7 +18,7 @@ const Contact = () => {
 
         <div className="mb-8">
           <h2 className="text-2xl font-bold mb-2 text-blue-700">Email Us</h2>
-          <p className="text-lg text-gray-700 mb-4">tca@visittabernacle.com</p>
+          <p className="text-lg text-gray-700 mb-4">info@norwalkbc.com</p>
           <a href="mailto:tca@visittabernacle.com?subject=Contact from Website">
             <button className="bg-blue-900 hover:bg-blue-700 text-white px-8 py-3 rounded text-xl">
               Email Now
@@ -28,8 +28,8 @@ const Contact = () => {
 
         <div className="mb-8">
           <h2 className="text-2xl font-bold mb-2 text-blue-700">We are at</h2>
-          <p className="text-lg text-gray-700 mb-4">929 Springfield Rd,<br/>Lebanon, MO 65536</p>
-          <a href="https://www.google.com/maps/place/Tabernacle+Baptist+Church/@37.6733476,-92.6702843,17z/data=!3m1!4b1!4m5!3m4!1s0x87c539e0a551f449:0x8d0b04d598ca99c8!8m2!3d37.6731641!4d-92.6681006?q=tabernacle+baptist+church+lebanon+mo&rlz=1C5CHFA_enIN983IN983&um=1&ie=UTF-8&sa=X&ved=2ahUKEwjTmNS4xJP5AhUAumMGHTmpBdgQ_AUoAXoECAIQAw&shorturl=1" target="_blank" rel="noopener noreferrer">
+          <p className="text-lg text-gray-700 mb-4">2084 US-20<br/>Norwalk, OH 44857</p>
+          <a href="https://www.google.com/maps/place/Norwalk+Baptist+Church/@41.2315543,-82.6689839,17z/data=!3m1!4b1!4m6!3m5!1s0x883a16f78e212c61:0x3f86fbfe677e041a!8m2!3d41.2315543!4d-82.666409!16s%2Fg%2F125_vdd0r?entry=ttu" target="_blank" rel="noopener noreferrer">
             <button className="bg-blue-900 hover:bg-blue-700 text-white px-8 py-3 rounded text-xl">
               Get Directions
             </button>
