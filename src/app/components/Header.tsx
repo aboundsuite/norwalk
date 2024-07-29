6c57e3028071b687b68734b9e5989d0104e9537b
Bernard Moses <mobile@aboundfi.com>
2024-07-29 09:36:14 +0530

    Initial commit

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -15,19 +15,21 @@ const Header = () => {
   };
 
   return (
-    <header className="fixed top-0 left-0 w-full bg-white text-white p-4 flex justify-between items-center z-50">
-      <div className="text-2xl font-bold"><a href='/'><img src='/nbc_logo.png' className='w-64'></img></a></div>
+    <header className="fixed top-0 left-0 w-full bg-white text-custom-dark-blue p-4 flex justify-between items-center z-50">
+      <div className="text-2xl font-bold">
+        <a href='/'><img src='/nbc_logo.png' className='w-64' alt="Norwalk Baptist Church Logo"></img></a>
+      </div>
       <button
-  className="text-2xl text-gray-800 focus:outline-none"
-  onClick={toggleMenu}
->
-  ☰
-</button>
+        className="text-2xl text-gray-800 focus:outline-none"
+        onClick={toggleMenu}
+      >
+        ☰
+      </button>
 
       {isOpen && (
         <div className="fixed inset-0 bg-custom-dark-blue flex flex-col items-center justify-center space-y-4 text-3xl transition-opacity duration-300 ease-in-out">
           <button
-            className="absolute top-4 right-4 text-2xl focus:outline-none"
+            className="absolute top-4 right-4 text-2xl text-white focus:outline-none"
             onClick={closeMenu}
           >
             &times;
@@ -35,24 +37,23 @@ const Header = () => {
           <nav>
             <ul className="space-y-4 text-center">
               <li>
-                <Link href="/about/#about" onClick={closeMenu}>
-                  About us
+                <Link href="/about/#about" legacyBehavior>
+                  <a className="text-white hover:text-custom-light-green" onClick={closeMenu}>About us</a>
                 </Link>
               </li>
               <li>
-                <Link href="/ministries" onClick={closeMenu}>
-                  Ministries
+                <Link href="/ministries" legacyBehavior>
+                  <a className="text-white hover:text-custom-light-green" onClick={closeMenu}>Ministries</a>
                 </Link>
               </li>
               <li>
-                <Link href="/contact" onClick={closeMenu}>
-                  Contact
+                <Link href="/contact" legacyBehavior>
+                  <a className="text-white hover:text-custom-light-green" onClick={closeMenu}>Contact</a>
                 </Link>
-                </li>
-                
-                <li>
-                <Link href="#join" onClick={closeMenu}>
-                  I’m new here
+              </li>
+              <li>
+                <Link href="#join" legacyBehavior>
+                  <a className="text-white hover:text-custom-light-green" onClick={closeMenu}>I’m new here</a>
                 </Link>
               </li>
             </ul>
