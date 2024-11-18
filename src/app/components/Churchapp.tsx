040da95a5990a9c87f4423a73557b81ce126cb1f
aboundsuite <148091793+aboundsuite@users.noreply.github.com>
2024-11-18 16:08:23 +0000

    first commit

--- src/app/components/Churchapp.tsx
+++ src/app/components/Churchapp.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ChurchApp = () => {
   return (
-    <section className="relative flex items-center justify-center min-h-screen bg-cover bg-center bg-blue-100" style={{ backgroundImage: 'url(/appdownload.jpg)' }}>
+    <section className="relative flex items-center justify-center min-h-screen bg-cover bg-center bg-blue-100" style={{ backgroundImage: 'url(/app-norwalkbaptistchurch.png)' }}>
       <div className="absolute inset-0 bg-black opacity-50"></div>
       <div className="relative z-10 text-center text-white p-4">
         <h1 className="text-4xl font-bold mb-4">Stay Connected 24x7</h1>
--- src/app/components/JoinUs.tsx
+++ src/app/components/JoinUs.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const JoinUs = () => (
   <section className="relative flex items-end justify-center min-h-screen bg-blue-100">
     <img
       className="absolute inset-0 w-full h-full object-cover"
-      src="/norwalk.png"
-     
+      src="/heronorwalk.png"
     />
     <div className="relative z-10 text-center text-white bg-blue-100 bg-opacity-50 p-10 rounded mb-20 drop-shadow-xl">
-
       <h1 className="text-xl font-bold mb-4"></h1>
-     
-      <button className="bg-blue-900 h-20 w-50 hover:bg-blue-700 text-white px-4 py-2 rounded px-20 text-xl">I’m new here!</button>
+
+      <button className="bg-blue-900 h-20 w-50 hover:bg-blue-700 text-white px-4 py-2 rounded px-20 text-xl">
+        I’m new here!
+      </button>
     </div>
   </section>
 );
